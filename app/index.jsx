917d39cdabef1362c4ac947c036416ad42b2e9ff
s3xmaxman <badside3919@gmail.com>
2024-05-01 13:01:14 +0900

    File & Folder Structure

--- app/index.jsx
+++ app/index.jsx
@@ -4,19 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Aora!</Text>
-      <StatusBar style="auto" />
-      <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
-    </View>
+      <View className="flex-1 items-center justify-center bg-white">
+        <Text className="text-3xl font-pblack">Aora!</Text>
+        <StatusBar style="auto" />
+        <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
+      </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
