6e04ec028ead5cba72f3464f0c2a834e79be5492
s3xmaxman <badside3919@gmail.com>
2024-05-05 21:37:33 +0900

    progress

---
+++ components/SearchInput.jsx
@@ -0,0 +1,5 @@
+import { useState } from "react";
+import { router, usePathname } from "expo-router";
+import { View, TouchableOpacity, Image, TextInput, Alert } from "react-native";
+
+import { icons } from "../constants";
