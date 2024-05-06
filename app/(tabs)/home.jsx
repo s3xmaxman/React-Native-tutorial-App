65eeb2b1370445ebca66d53d5fde86983e6eeea8
s3xmaxman <badside3919@gmail.com>
2024-05-06 19:00:17 +0900

    progress

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,19 @@
-import { View, Text, FlatList, Image } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
+import React, { useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
+import Trending from '../../components/Trending'
+import EmptyState from '../../components/EmptyState'
 
 const Home = () => {
+  
+  const [refreshing, setRefreshing] = useState(false);
+  
+  const onRefresh = async () => {
+    setRefreshing(true);  
+  };
   return (
     <SafeAreaView className="bg-primary">
       <FlatList
@@ -30,9 +38,22 @@ const Home = () => {
                 />
               </View>
             </View>
+
             <SearchInput />
+
+            <View className="w-full flex-1 pt-5 pb-8">
+              <Text className="text-gray-100 text-lg font-pregular mb-3">
+                 Latest Videos
+              </Text>
+
+              <Trending posts={[{ id:1 }, { id:2 }, { id:3 }] ?? []} />
+            </View>
           </View>
         )}
+        ListEmptyComponent={() => (
+          <EmptyState title="No videos found"  subtitle="No videos created yet" />
+        )}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>}
       >
 
       </FlatList>
