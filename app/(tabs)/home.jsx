b08a89f394c54b4812fa13385b2c901952627470
s3xmaxman <badside3919@gmail.com>
2024-05-06 19:50:45 +0900

    progress

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,19 +1,26 @@
-import { View, Text, FlatList, Image, RefreshControl } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, FlatList, Image, RefreshControl, Alert } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 import { images } from '../../constants'
 import SearchInput from '../../components/SearchInput'
 import Trending from '../../components/Trending'
 import EmptyState from '../../components/EmptyState'
+import { getAllPosts } from '../../lib/appwrite'
+import useAppwrite from '../../lib/useAppwrite'
 
 const Home = () => {
-  
+  const { data: posts } = useAppwrite(getAllPosts);
+
   const [refreshing, setRefreshing] = useState(false);
-  
+
   const onRefresh = async () => {
     setRefreshing(true);  
   };
+
+  console.log(posts)
+
+
   return (
     <SafeAreaView className="bg-primary">
       <FlatList
